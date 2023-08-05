df657c414c253cd48f5b22aa47d6433736b76c16
vrcalomino <vrcalomino@gmail.com>
2023-08-04 23:30:56 -0300

    backend environment variables rerouting

--- backend-ranking/utils/config.js
+++ backend-ranking/utils/config.js
@@ -1,4 +1,15 @@
-require("dotenv").config();
+const fs = require('fs');
+const path = require('path');
+const dotenv = require('dotenv');
+
+// Construye la ruta absoluta al archivo .env desde la raíz del repositorio
+const envPath = path.join(__dirname, '.env');
+
+// Lee el contenido del archivo .env si existe
+if (fs.existsSync(envPath)) {
+  const envContent = fs.readFileSync(envPath, 'utf8');
+  dotenv.parse(envContent);
+}
 
 const PORT = process.env.PORT;
 const MONGODB_URI = process.env.MONGODB_URI;
